02c7c19d1b127b62e7e8a2814044779cf4bf823e
Darma Wiryanata <45613994+DarmaWiryanata@users.noreply.github.com>
2022-12-23 11:47:48 +0800

    Initial Commit

---
+++ NontonKuy/NontonKuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NontonKuyApp.swift
+//  NontonKuy
+//
+//  Created by Darma Wiryanata on 23/12/22.
+//
+
+import SwiftUI
+
+@main
+struct NontonKuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
