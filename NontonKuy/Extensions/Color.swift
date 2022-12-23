15466d244508f89affbb739df1dc7bb03258cbea
Darma Wiryanata <45613994+DarmaWiryanata@users.noreply.github.com>
2022-12-23 22:39:05 +0800

    Color extension

---
+++ NontonKuy/Extensions/Color.swift
@@ -0,0 +1,19 @@
+//
+//  Color.swift
+//  NontonKuy
+//
+//  Created by Darma Wiryanata on 23/12/22.
+//
+
+import SwiftUI
+
+extension Color {
+    static let ui = UI()
+    
+    struct UI {
+        let accent = Color("AccentColor")
+        let background = Color("BackgroundColor")
+        let primaryText = Color("PrimaryTextColor")
+        let secondaryText = Color("SecondaryTextColor")
+    }
+}
