7bc0b62d697e8094be4ae986de6f894d61aa849f
Darma Wiryanata <45613994+DarmaWiryanata@users.noreply.github.com>
2022-12-25 07:36:56 +0800

    Movie cell view

--- NontonKuy/Views/Components/MovieCellView.swift
+++ NontonKuy/Views/Components/MovieCellView.swift
@@ -11,44 +11,49 @@ struct MovieCellView: View {
     let rank: String
     
     var body: some View {
-        VStack(alignment: .leading) {
-            ZStack(alignment: .topTrailing) {
-                Image.ui.samplePoster
-                    .resizable()
-                    .scaledToFit()
-                    .clipShape(RoundedRectangle(cornerRadius: 20))
-                
-                if rank != "" {
-                    Text(rank)
-                        .font(.callout)
-                        .padding()
-                        .overlay {
-                            Circle()
-                                .stroke(
-                                    Color.ui.accent,
-                                    style: StrokeStyle(lineWidth: 3)
-                                )
-                                .foregroundColor(Color.ui.background)
-                        }
-                        .background {
-                            Circle()
-                                .foregroundColor(Color.ui.background)
-                        }
-                        .padding(.trailing, 8)
+        NavigationLink {
+            MovieInfoScreen(title: "Avatar")
+        } label: {
+            VStack(alignment: .leading) {
+                ZStack(alignment: .topTrailing) {
+                    Image.ui.samplePoster
+                        .resizable()
+                        .scaledToFit()
+                        .clipShape(RoundedRectangle(cornerRadius: 20))
+                    
+                    if rank != "" {
+                        Text(rank)
+                            .font(.callout)
+                            .padding()
+                            .overlay {
+                                Circle()
+                                    .stroke(
+                                        Color.ui.accent,
+                                        style: StrokeStyle(lineWidth: 3)
+                                    )
+                                    .foregroundColor(Color.ui.background)
+                            }
+                            .background {
+                                Circle()
+                                    .foregroundColor(Color.ui.background)
+                            }
+                            .padding(.trailing, 8)
+                    }
                 }
+                .padding(.trailing)
+                
+                Text("Avatar: The Way of Water")
+                    .font(.title3)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color.ui.primaryText)
+                    .multilineTextAlignment(.leading)
+                    .padding(.top, 8)
+                
+                Text("Action, Adventure")
+                    .foregroundColor(Color.ui.secondaryText)
             }
-            .padding(.trailing)
-            
-            Text("Avatar: The Way of Water")
-                .font(.title3)
-                .fontWeight(.semibold)
-                .foregroundColor(Color.ui.primaryText)
-                .padding(.top, 8)
-            
-            Text("Action, Adventure")
-                .foregroundColor(Color.ui.secondaryText)
+            .frame(width: 175)
         }
-        .frame(width: 175)
     }
 }
 
