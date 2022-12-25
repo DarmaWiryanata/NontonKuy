01d281b4313e5cd82fdf4c6735d0eafec6157bf0
Darma Wiryanata <45613994+DarmaWiryanata@users.noreply.github.com>
2022-12-25 21:33:20 +0800

    No reviewes handling

--- NontonKuy/Views/Screens/MovieInfo/Components/ReviewsView.swift
+++ NontonKuy/Views/Screens/MovieInfo/Components/ReviewsView.swift
@@ -14,45 +14,50 @@ struct ReviewsView: View {
         VStack(alignment: .leading) {
             SectionHeaderView(title: "Review")
             
-            ForEach(vm.movieReviews) { review in
-                HStack {
-                    // Author avatar
-                    if let avatarPath = review.authorDetails.avatarPath {
-                        AsyncImage(url: URL(string: "https://image.tmdb.org/t/p/w500/\(avatarPath)")!) { image in
-                            image
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 30, height: 30)
-                                .clipShape(Circle())
-                        } placeholder: {
-                            Circle()
-                                .foregroundColor(Color.ui.background)
-                                .frame(width: 30, height: 30)
-                                .overlay {
-                                    Image(systemName: "person.fill")
-                                }
+            if vm.movieReviews.count > 0 {
+                ForEach(vm.movieReviews) { review in
+                    HStack {
+                        // Author avatar
+                        if let avatarPath = review.authorDetails.avatarPath {
+                            AsyncImage(url: URL(string: "https://image.tmdb.org/t/p/w500/\(avatarPath)")!) { image in
+                                image
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 30, height: 30)
+                                    .clipShape(Circle())
+                            } placeholder: {
+                                Circle()
+                                    .foregroundColor(Color.ui.background)
+                                    .frame(width: 30, height: 30)
+                                    .overlay {
+                                        Image(systemName: "person.fill")
+                                    }
+                            }
+                        }
+                        
+                        // Author name
+                        HStack {
+                            Text(review.author)
+                                .multilineTextAlignment(.leading)
+                            
+                            SeparatorView()
+                                .padding(.horizontal, 8)
+                            
+                            RateView(value: review.authorDetails.rating)
                         }
                     }
                     
-                    // Author name
-                    HStack {
-                        Text(review.author)
-                            .multilineTextAlignment(.leading)
-                        
-                        SeparatorView()
-                            .padding(.horizontal, 8)
-                        
-                        RateView(value: review.authorDetails.rating)
-                    }
+                    Text(review.content)
+                        .font(.footnote)
+                        .multilineTextAlignment(.leading)
+                    
+                    Divider()
                 }
-                
-                Text(review.content)
-                    .font(.footnote)
-                    .multilineTextAlignment(.leading)
-                
-                Divider()
+                .padding(.horizontal)
+            } else {
+                Text("No review yet")
+                    .frame(maxWidth: .infinity)
             }
-            .padding(.horizontal)
         }
     }
 }
